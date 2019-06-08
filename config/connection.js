298e4e1ac8afd4147732d1fd8e7b76632298f495
Paola <paola.obreguillermo@yahoo.com>
2019-06-08 15:30:44 -0500

    changes 1

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer");
+var connection;
 
+if (process.env.JAWSDB_URL) {
 
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+   
+   } else {
+   
+	connection = mysql.createConnection({
+   
+	  host: "localhost",
+	  user: "paola",
+	  password: "paola12152000",
+	  database: "burgers_db"
+   
+	});
+   
+   };
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "paola",
-	password: "paola12152000",
-	database: "burgers_db"
-})
-
-connection.connect(function (err) {
-	if (err) {
-		console.error("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();   
 module.exports = connection;
 
